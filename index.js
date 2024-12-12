b101224e18dccd20e49760eead6a7463488d2f07
Animesh Shukla <animesh.shukla@outbox.vc>
2024-12-12 12:32:31 +0530

    feat: added body parsing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -38,7 +39,7 @@ app.post('/webhook', (req, res) => {
     }
     
     // Return a response to acknowledge receipt of the event
-    res.json({ received: true });
+    res.json({ received: true ,event,eventType: event.type});
     });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
